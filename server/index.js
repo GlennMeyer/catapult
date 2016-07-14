e032e249522667caa2e7eb9bc9f91b1e266390fa
Glenn Meyer <gmeyer@metalnetworks.com>
2016-07-14 17:12:23 -0500

    express-static initial commit

--- server/index.js
+++ server/index.js
@@ -4,15 +4,20 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var hostname = '127.0.0.1';
 var port = 3000;
 
 var app = (0, _express2.default)();
+var assetFolder = _path2.default.resolve(__dirname, '../client/public');
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.sendFile(assetFolder + '/index.html');
 });
 
 app.listen(port, hostname, function () {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const app = express();
+const assetFolder = path.resolve(__dirname, '../client/public');
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.sendFile(`${assetFolder}/index.html`);
 });
 
 app.listen(port, hostname, () => {
